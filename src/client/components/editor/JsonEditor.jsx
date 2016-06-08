78f56ed7b9d1884aa47a806cd45d02d8a2f763a9
Maik Marschner <an@maik.tk>
2016-06-08 16:24:34 +0200

    (feat) Add code folding in json views.

--- src/client/components/editor/JsonEditor.jsx
+++ src/client/components/editor/JsonEditor.jsx
@@ -2,17 +2,20 @@ import * as React from 'react'
 import brace from 'brace'
 import AceEditor from 'react-ace'
 
-import 'brace/mode/javascript'
+import 'brace/mode/json'
 import 'brace/theme/github'
 
 export default function (props) {
   return (
     <AceEditor
       {...props}
-      mode='lisp'
+      mode='json'
       theme='github'
       name={props.uniqueId}
-      editorProps={{$blockScrolling: true}}
+      setOptions={{
+        showFoldWidgets: true,
+        foldStyle: 'markbegin'
+      }}
       width='100%'
       height='100%'
     />
