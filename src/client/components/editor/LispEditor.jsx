6020e64d5ec80da8ba0a0b2da47b8eb7a1337d38
Maik Marschner <mmarschner@gdv.uni-hannover.de>
2016-06-10 15:22:11 +0200

    (fix) Use clojure syntax highlighting instead of lisp.

--- src/client/components/editor/LispEditor.jsx
+++ src/client/components/editor/LispEditor.jsx
@@ -2,14 +2,14 @@ import * as React from 'react'
 import brace from 'brace'
 import AceEditor from 'react-ace'
 
-import 'brace/mode/lisp'
+import 'brace/mode/clojure'
 import 'brace/theme/github'
 
 export default function (props) {
   return (
     <AceEditor
       {...props}
-      mode='lisp'
+      mode='clojure'
       theme='github'
       name={props.uniqueId}
       editorProps={{$blockScrolling: Infinity}}
