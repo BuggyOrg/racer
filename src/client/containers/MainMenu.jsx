b752a4333f34d9cc9cc9e84550e6dbe40eeaa845
Maik Marschner <an@maik.tk>
2016-08-24 22:09:13 +0200

    (feat) Add dummy handlers to the menu and connect it with redux.

--- src/client/containers/MainMenu.jsx
+++ src/client/containers/MainMenu.jsx
@@ -5,8 +5,21 @@ import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
 import Divider from 'material-ui/Divider'
 import Folder from 'material-ui/svg-icons/file/folder'
 import Save from 'material-ui/svg-icons/content/save'
+import { connect } from 'react-redux'
 
-export default class extends React.Component {
+class MainMenu extends React.Component {
+  handleOpenFile () {
+    console.log('open')
+  }
+
+  handleSaveFile () {
+    console.log('save')
+  }
+
+  handleExportGraph () {
+    console.log('export graph')
+  }
+
   render () {
     return (
       <IconMenu
@@ -16,13 +29,40 @@ export default class extends React.Component {
         menuStyle={{ width: 200 }}
         desktop
       >
-        <MenuItem primaryText="Open" secondaryText="Ctrl + O" leftIcon={<Folder />} />
-        <MenuItem primaryText="Save as" secondaryText="Ctrl + S" leftIcon={<Save />} />
+        <MenuItem
+          primaryText="Open"
+          secondaryText="Ctrl + O"
+          leftIcon={<Folder />}
+          onTouchTap={() => this.handleOpenFile()}
+        />
+        <MenuItem
+          primaryText="Save as"
+          secondaryText="Ctrl + S"
+          leftIcon={<Save />}
+          onTouchTap={() => this.handleSaveFile()}
+        />
         <Divider />
-        <MenuItem primaryText="Export graph" insetChildren />
+        <MenuItem
+          primaryText="Export graph" 
+          insetChildren
+          onTouchTap={() => this.handleExportGraph()}
+        />
         <Divider />
-        <MenuItem primaryText="Power mode" insetChildren />
+        <MenuItem
+          primaryText="Power mode"
+          insetChildren
+          disabled
+          checked={this.props.powerMode}
+        />
       </IconMenu>
     )
   }
 }
+
+export default connect((state) => {
+  return {
+    code: state.code,
+    controlFlowGraph: state.controlFlowGraph,
+    powerMode: false
+  }
+})(MainMenu)
