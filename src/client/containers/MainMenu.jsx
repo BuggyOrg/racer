458fe89c5362be43034b3a58b505c86e5004949e
leMaik <an@maik.tk>
2016-08-24 23:18:44 +0200

    (feat) Implement opening files and keyboard shortcuts.

--- src/client/containers/MainMenu.jsx
+++ src/client/containers/MainMenu.jsx
@@ -6,10 +6,37 @@ import Divider from 'material-ui/Divider'
 import Folder from 'material-ui/svg-icons/file/folder'
 import Save from 'material-ui/svg-icons/content/save'
 import { connect } from 'react-redux'
+import { setLispCode, compileProgram } from '../actions'
 
 class MainMenu extends React.Component {
-  handleOpenFile () {
-    console.log('open')
+  componentDidMount () {
+    window.addEventListener('keydown', this.handleShortcutKeys)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('keydown', this.handleShortcutKeys)
+  }
+
+  handleShortcutKeys = (e) => {
+    if (e.key === 'o' && e.ctrlKey) {
+      this.openInput.click()
+      e.preventDefault()
+    } else if (e.key === 's' && e.ctrlKey) {
+      this.handleSaveFile()
+      e.preventDefault()
+    }
+  }
+
+  handleOpenFile (e) {
+    const files = e.target.files
+    if (files.length > 0) {
+      const reader = new FileReader()
+      reader.addEventListener('loadend', (event) => {    
+        this.props.dispatch(setLispCode(reader.result))
+        this.props.dispatch(compileProgram())
+      })
+      reader.readAsText(files[0], 'utf8')
+    }
   }
 
   handleSaveFile () {
@@ -22,39 +49,51 @@ class MainMenu extends React.Component {
 
   render () {
     return (
-      <IconMenu
-        {...this.props}
-        anchorOrigin={{horizontal: 'right', vertical: 'center'}}
-        targetOrigin={{horizontal: 'right', vertical: 'top'}}
-        menuStyle={{ width: 200 }}
-        desktop
-      >
-        <MenuItem
-          primaryText="Open"
-          secondaryText="Ctrl + O"
-          leftIcon={<Folder />}
-          onTouchTap={() => this.handleOpenFile()}
-        />
-        <MenuItem
-          primaryText="Save as"
-          secondaryText="Ctrl + S"
-          leftIcon={<Save />}
-          onTouchTap={() => this.handleSaveFile()}
-        />
-        <Divider />
-        <MenuItem
-          primaryText="Export graph" 
-          insetChildren
-          onTouchTap={() => this.handleExportGraph()}
-        />
-        <Divider />
-        <MenuItem
-          primaryText="Power mode"
-          insetChildren
-          disabled
-          checked={this.props.powerMode}
-        />
-      </IconMenu>
+      <div>
+        <IconMenu
+          {...this.props}
+          anchorOrigin={{horizontal: 'right', vertical: 'center'}}
+          targetOrigin={{horizontal: 'right', vertical: 'top'}}
+          menuStyle={{ width: 200 }}
+          desktop
+        >
+          <MenuItem
+            primaryText="Open"
+            secondaryText="Ctrl + O"
+            leftIcon={<Folder />}
+            onTouchTap={(e) => { this.openInput.click(); e.stopPropagation() }}
+          />
+          <MenuItem
+            primaryText="Save as"
+            secondaryText="Ctrl + S"
+            leftIcon={<Save />}
+            onTouchTap={() => this.handleSaveFile()}
+          />
+          <Divider />
+          <MenuItem
+            primaryText="Export graph" 
+            insetChildren
+            onTouchTap={() => this.handleExportGraph()}
+          />
+          <Divider />
+          <MenuItem
+            primaryText="Power mode"
+            insetChildren
+            disabled
+            checked={this.props.powerMode}
+          />
+        </IconMenu>
+        <div
+          style={{ position: 'fixed', top: -200 }}
+        >
+          <input
+            type="file"
+            ref={(ref) => this.openInput = ref}
+            onChange={(e) => this.handleOpenFile(e)}
+            accept=".clj,text/*"
+          />
+        </div>
+      </div>
     )
   }
 }
