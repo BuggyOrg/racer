32b3e63f60489744c1e6480f31a2745077f1703f
Maik Marschner <mmarschner@gdv.uni-hannover.de>
2017-03-09 15:05:31 +0100

    (cleanup) Remove graphify worker.

--- src/server/forkGraphifyWorker.js
+++
@@ -1,7 +0,0 @@
-import graphify from '@buggyorg/graphify'
-
-process.on('message', (m) => {
-    graphify(JSON.parse(m))
-    .then((svg) => process.send(JSON.stringify({ svg })))
-    .catch((e) => process.send(JSON.stringify({ error: e })))
-})
