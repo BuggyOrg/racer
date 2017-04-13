56d7ceb29e620b40150d9fdc497b62eb75cd00f1
Maik Marschner <mmarschner@gdv.uni-hannover.de>
2017-04-13 12:22:28 +0200

    (fix) Abort compilation request before compiling new code.

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -5,13 +5,17 @@ import { SET_RESOLVED_GRAPH, SET_CODE, SET_RESOLVED_GRAPH_LOADING,
          SET_CONTROL_FLOW_GRAPH, SET_CONTROL_FLOW_GRAPH_LOADING,
          SET_CODE_ERRORS, TOGGLE_POWER_MODE } from './constants'
 
+let previousCompileRequest
 export function compileProgram (code) {
   return (dispatch) => {
     dispatch({ type: SET_RESOLVED_GRAPH_LOADING, loading: true })
     dispatch({ type: SET_UNRESOLVED_GRAPH_LOADING, loading: true })
     dispatch({ type: SET_CONTROL_FLOW_GRAPH_LOADING, loading: true })
 
-    qwest.post('/api/lisgy/parse?type=all', new Blob([code], { type: 'text/plain' }))
+    if (previousCompileRequest != null) {
+      previousCompileRequest.abort()
+    }
+    previousCompileRequest = qwest.post('/api/lisgy/parse?type=all', new Blob([code], { type: 'text/plain' }))
     .then((xhr, { unresolved, resolved, svg }) => {
       dispatch({ type: SET_CODE_ERRORS, errors: [] })
       dispatch({ type: SET_UNRESOLVED_GRAPH, graph: unresolved })
